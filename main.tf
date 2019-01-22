f0247af7f0f9584114567740d93c9aa26bc59503
Mark J. Reed <mark.reed@turner.com>
2019-01-22 15:30:07 -0500

    Turn on AES256 encryption by default

--- main.tf
+++ main.tf
@@ -25,6 +25,14 @@ resource "aws_s3_bucket" "bucket" {
   }
 
   tags = "${var.tags}"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 }
 
 # lookup the role arn
